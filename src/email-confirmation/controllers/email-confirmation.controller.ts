097e77f8d8586ddf2e3d0dc91b9c554a8eff9052
Alex <andutzu.268@gmail.com>
2023-11-17 15:18:17 +0200

    password reset through token

--- src/email-confirmation/controllers/email-confirmation.controller.ts
+++ src/email-confirmation/controllers/email-confirmation.controller.ts
@@ -5,17 +5,20 @@ import { UserDto } from 'src/users/dto/user.dto';
 import { map } from 'rxjs';
 import { EmailConfirmationBypassed } from '../decorators/email-confirmation-bypassed.decorator';
 import { plainToInstance } from 'class-transformer';
+import { UsersService } from 'src/users/services/users.service';
 
 @Controller('email-confirmation')
 export class EmailConfirmationController {
   constructor(
     private readonly _emailConfirmationService: EmailConfirmationService,
+    private readonly _usersService: UsersService,
   ) {}
 
-  @Post('confirm')
+  @Post('email-confirm')
   @EmailConfirmationBypassed()
   confirm(@Query('token') token: string, @Res() res: Response) {
-    const email = this._emailConfirmationService.decodeConfirmationToken(token);
+    const email: string =
+      this._emailConfirmationService.decodeConfirmationToken(token);
     return this._emailConfirmationService.confirmEmail(email).pipe(
       map((user) => plainToInstance(UserDto, user)),
       map((user) => {
@@ -32,6 +35,18 @@ export class EmailConfirmationController {
     return this._emailConfirmationService.resendVerificationLink(user).pipe(
       map(() => ({
         message: `Verification link resent to ${user.email}`,
+      })),
+    );
+  }
+
+  @Post('password-reset-confirm')
+  updatePassword(@Query('token') token: string, @Req() req: Request) {
+    const hashedPassword: string =
+      this._emailConfirmationService.decodePasswordResetToken(token);
+    const user = req.user as UserDto;
+    return this._usersService.updatePassword(user.id, hashedPassword).pipe(
+      map(() => ({
+        message: 'Password updated successfully',
       })),
     );
   }
